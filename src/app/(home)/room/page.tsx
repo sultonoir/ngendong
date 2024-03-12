0ebe386aad9ddf146a82cd805d03db41782d54c4
Sulton <sultonalidrus5@gmail.com>
2024-03-12 18:39:46 +0700

    test

--- src/app/(home)/room/page.tsx
+++ src/app/(home)/room/page.tsx
@@ -1,17 +1,40 @@
 "use client";
-import RoomCard from "@/components/template/room/RoomCard";
+import TestForm from "@/components/template/form/TestForm";
 import { api } from "@/trpc/react";
+import Image from "next/image";
 import React from "react";
 
-const Page = () => {
-  const { data: rooms } = api.post.getAllRooms.useQuery();
+export const dynamic = "force-dynamic";
+const page = () => {
+  const { data, isLoading } = api.post.getAllNovel.useQuery();
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div className="container pt-10 lg:h-[95dvh]">
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-5">
-        {rooms?.map((item) => <RoomCard key={item.id} room={item} />)}
+    <div className="container pt-10">
+      <TestForm />
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-6">
+        {!data ? null : (
+          <>
+            {data.map((item) => (
+              <div key={item.id} className="flex flex-col gap-2">
+                <Image
+                  alt={item.title}
+                  src={item.image ?? "/logo.png"}
+                  className="aspect-square object-cover"
+                  priority
+                  width={200}
+                  height={200}
+                  loading="eager"
+                />
+                <p className="truncate text-lg leading-none">{item.title}</p>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-export default Page;
+export default page;
